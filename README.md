65ba0c32f4582270719b9f90c2ba6c0b94f1691b
Alex Titarenko <alex.titarenko@talex-soft.com>
2015-03-09 20:10:03 +0200

    update README.md add unit tests project

--- README.md
+++ README.md
@@ -3,6 +3,42 @@
 
 Lightweight MVVM framework for C#.
 
+## Structure
+* **TAlex.Mvvm** - provides base view model and relay command classes.
+* **TAlex.Mvvm.Wpf** - WPF depended implementation of message service, application class extensions and some commands.
+
+## Example of usage
+Defining view model:
+```C#
+// View Model
+public class TestViewModel : ViewModelBase
+{
+    private int _intProp;
+    private string _strProp;
+
+
+    public int IntegerProperty
+    {
+        get { return _intProp; }
+        set { Set(ref _intProp, value); }
+    }
+
+    public string StringProperty
+    {
+        get { return _strProp; }
+        set { Set(() => StringProperty, ref _strProp, value); }
+    }
+}
+
+// PropertyChanged event
+var target = new TestViewModel();
+string actualPropName = null;
+target.PropertyChanged += (e, a) => { actualPropName = a.PropertyName; };
+
+target.IntegerProperty = 3;
+
+Console.WriteLine(actualPropName);
+```
 
 ## Get it on NuGet!
 
---
+++ TAlex.Mvvm.Tests/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TAlex.Mvvm.ViewModels;
+
+
+namespace TAlex.Mvvm.Tests.ViewModels
+{
+    [TestFixture]
+    public class ViewModelBaseTests
+    {
+        protected TestViewModel Target;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            Target = new TestViewModel();
+        }
+
+
+        #region PropertyChanged
+
+        [Test]
+        public void PropertyChanged_SetIntegerProperty_RaisePropertyChanged()
+        {
+            //arrange
+            const int value = 3;
+            string actualPropName = null;
+            Target.PropertyChanged += (e, a) => { actualPropName = a.PropertyName; }; 
+
+            //action
+            Target.IntegerProperty = value;
+
+            //assert
+            Assert.AreEqual(value, Target.IntegerProperty);
+            Assert.AreEqual("IntegerProperty", actualPropName);
+        }
+
+        [Test]
+        public void PropertyChanged_SetStringProperty_RaisePropertyChanged()
+        {
+            //arrange
+            const string value = "hello world";
+            string actualPropName = null;
+            Target.PropertyChanged += (e, a) => { actualPropName = a.PropertyName; };
+
+            //action
+            Target.StringProperty = value;
+
+            //assert
+            Assert.AreEqual(value, Target.StringProperty);
+            Assert.AreEqual("StringProperty", actualPropName);
+        }
+
+        [Test]
+        public void PropertyChanged_SetTheSameIntegerPropertyValue_DoNotRaisePropertyChanged()
+        {
+            //arrange
+            const int value = 3;
+            string actualPropName = null;
+            Target.IntegerProperty = value;
+            Target.PropertyChanged += (e, a) => { actualPropName = a.PropertyName; };
+
+            //action
+            Target.IntegerProperty = value;
+
+            //assert
+            Assert.AreEqual(value, Target.IntegerProperty);
+            Assert.AreEqual(null, actualPropName);
+        }
+
+        #endregion
+
+
+        #region Design Data
+
+        public class TestViewModel : ViewModelBase
+        {
+            private int _intProp;
+            private string _strProp;
+
+
+            public int IntegerProperty
+            {
+                get { return _intProp; }
+                set { Set(ref _intProp, value); }
+            }
+
+            public string StringProperty
+            {
+                get { return _strProp; }
+                set { Set(() => StringProperty, ref _strProp, value); }
+            }
+        }
+
+        #endregion
+    }
+}
